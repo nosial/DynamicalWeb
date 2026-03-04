da12fb395635e4d7f7e4d184237c5be55f3f5b51
netkas <netkas.nosial@gmail.com>
2026-03-04 15:35:01 -0500

    Add SectionException class with constructor for improved exception handling

---
+++ src/DynamicalWeb/Exceptions/SectionException.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace DynamicalWeb\Exceptions;
+
+    use Throwable;
+
+    class SectionException extends DynamicalWebException
+    {
+        /**
+         * @inheritDoc
+         */
+        public function __construct(string $message="", int $code=0, ?Throwable $previous=null)
+        {
+            parent::__construct($message, $code, $previous);
+        }
+    }
