cc1628978420deece172197e8602cc29be02700c
netkas <netkas.nosial@gmail.com>
2026-03-04 19:21:02 -0500

    Add singleton logger instance retrieval to Logger class for improved logging management

--- src/DynamicalWeb/Classes/Logger.php
+++ src/DynamicalWeb/Classes/Logger.php
@@ -1,8 +1,23 @@
 <?php
 
-namespace DynamicalWeb\Classes;
+    namespace DynamicalWeb\Classes;
 
-class Logger
-{
+    class Logger
+    {
+        private static ?\LogLib2\Logger $logger=null;
 
-}
+        /**
+         * Retrieves the logger instance for the DynamicalWeb framework.
+         *
+         * @return \LogLib2\Logger Returns the logger instance.
+         */
+        public static function getLogger(): \LogLib2\Logger
+        {
+            if(self::$logger === null)
+            {
+                self::$logger = new \LogLib2\Logger('net.nosial.dynamicalweb');
+            }
+
+            return self::$logger;
+        }
+    }
